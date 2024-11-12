90d0c18a86b29ea9d7cf32bfa1bc020569c4a180
Бажен <95105368+Tam15937@users.noreply.github.com>
2024-11-12 17:52:22 +0700

    upgrade

--- sketch_oct5a_copy_20241109124833/sketch_oct5a_copy_20241109124833.ino
+++ sketch_oct5a_copy_20241109124833/sketch_oct5a_copy_20241109124833.ino
@@ -1,57 +1,20 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-
-
+const int stepPins[] = {28, 26, 24, 22, 23, 25, 27, 29};
+const int dirPins[] = {36, 34, 32, 30, 31, 33, 35, 37};
+const int stopoPins[] = {42, 44, 46, 48, 49, 47, 45, 43};
 #define enable 40
 #define stool 7
 
-#define stepm1 28
-#define dirm1 36
-#define stopo1 42
 
-#define stepm2 26
-#define dirm2 34
-
-#define stepm3 24
-#define dirm3 32
-
-#define stepm4 22
-#define dirm4 30
-
-#define stepm5 23
-#define dirm5 31
-
-#define stepm6 25
-#define dirm6 33
-
-#define stepm7 27
-#define dirm7 35
-
-#define stepm8 29
-#define dirm8 37
 
 void setup() {
   // Настраиваем пины как выходы
-  pinMode(22, OUTPUT);  pinMode(23, OUTPUT);
-  pinMode(24, OUTPUT);  pinMode(25, OUTPUT);
-  pinMode(26, OUTPUT);  pinMode(27, OUTPUT);
-  pinMode(28, OUTPUT);  pinMode(29, OUTPUT);
-  pinMode(30, OUTPUT);  pinMode(31, OUTPUT);
-  pinMode(32, OUTPUT);  pinMode(33, OUTPUT);
-  pinMode(34, OUTPUT);  pinMode(35, OUTPUT);
-  pinMode(36, OUTPUT);  pinMode(37, OUTPUT);
-  pinMode(38, OUTPUT);  pinMode(39, OUTPUT);
-  pinMode(40, OUTPUT);  pinMode(41, OUTPUT);
-  pinMode(42, INPUT);  pinMode(43, OUTPUT);
-  pinMode(44, OUTPUT);  pinMode(45, OUTPUT);
-  pinMode(46, OUTPUT);  pinMode(47, OUTPUT);
-  pinMode(48, OUTPUT);  pinMode(49, OUTPUT);
-  pinMode(50, OUTPUT);  pinMode(51, OUTPUT);
-  pinMode(52, OUTPUT);  pinMode(53, OUTPUT);
-
+  for(int i=22;i<50;i++){
+    i<42? pinMode(i, OUTPUT) : pinMode(i, INPUT);
+  }
   digitalWrite(enable, LOW);
-
 }
 
 void microDelay(int k){
@@ -62,56 +25,21 @@ void microDelay(int k){
   }
 }
 
-void step(int m, bool r) {
-  int t_s;
-  int t_d;
-  switch (m) {
-    case 1:
-      t_s = stepm1;
-      t_d = dirm1;
-      break;
-    case 2:
-      t_s = stepm2;
-      t_d = dirm2;
-      break;
-    case 3:
-      t_s = stepm3;
-      t_d = dirm3;
-      break;
-    case 4:
-      t_s = stepm4;
-      t_d = dirm4;
-      break;
-    case 5:
-      t_s = stepm5;
-      t_d = dirm5;
-      break;
-    case 6:
-      t_s = stepm6;
-      t_d = dirm6;
-      break;
-    case 7:
-      t_s = stepm7;
-      t_d = dirm7;
-      break;
-    case 8:
-      t_s = stepm8;
-      t_d = dirm8;
-      break;
-  }
-  r == 0 ? digitalWrite(t_d, LOW) : digitalWrite(t_d, HIGH);
-  digitalWrite(t_s, LOW);
-  microDelay(24);//24
-  digitalWrite(t_s, HIGH);
-  microDelay(24); 
+void step(int motorIndex, bool direction) {
+    if (motorIndex < 1 || motorIndex > 8) return; // Проверка на допустимый индекс
+    int t_s = stepPins[motorIndex - 1];
+    int t_d = dirPins[motorIndex - 1];
+
+    digitalWrite(t_d, direction ? HIGH : LOW);
+    digitalWrite(t_s, LOW);
+    microDelay(23);//23 optimal 21 fast
+    digitalWrite(t_s, HIGH);
+    microDelay(23); 
 }
 void loop() {
-  if(digitalRead(stopo1)==LOW){
-    step(1,0);
-    step(2,0);
-    step(3,0);
-    step(4,0);
-    step(5,0);
-    step(6,0);
+  if(digitalRead(stopoPins[0])==LOW){
+    for (int i = 1; i <= 6; i++) {
+        step(i, false);
+    }
   }
 }
